8a8d850db3acb94862db49fb773e4581f3e083f3
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-07-25 18:53:10 +0300

    Update to net7.0 to enable IParsable<>

--- UnoTableDb/Core/BaseModel.cs
+++ UnoTableDb/Core/BaseModel.cs
@@ -19,40 +19,47 @@ public abstract class BaseModel
     {
         Id = id;
     }
-
+    
+    /// <summary>
+    /// Checks whether a model class is valid for proper parsing
+    /// </summary>
+    /// <param name="modelType"> The type of the model class</param>
+    /// <remarks>Model is considered valid if:<br />
+    /// 1. It derives from a BaseModel class <br />
+    /// 2. It has a one-parameter constructor with Guid parameter <br />
+    /// 3. The types of properties tracked should implement IParsable <br />
+    /// 4. All tracked properties should have public get, set <br />
+    /// 5. Only one property with [PrimaryKey] which hasn't got a [NotTracking]</remarks>
     public static bool IsModelTypeValid(Type modelType)
     {
-        // Model is considered valid if:
-        // * There should be a one-parameter constructor with bool parameter "generateId"
-        // TODO: * All All tracked properties should hold parsable value (Implement IParsable<T>)
-        // * All tracked properties should have public get and set
-        // * Only one property with [PrimaryKey] and it hasn't got a [NotTracking]
-
-        if (modelType.GetConstructors().
+        if (!modelType.IsSubclassOf(typeof(BaseModel)))
+        {
+            return false;
+        }
+        
+        if (!modelType.GetConstructors().
             Select(c => c.GetParameters())
-            .Any(ps => ps.Length == 1 &&
-                       ps[0].ParameterType == typeof(bool) &&
-                       ps[0].Name == "generateId"))
+            .Any(ps => ps.Length == 1 && ps[0].ParameterType == typeof(Guid)))
         {
             return false;
         }
-
-        var properties = modelType.GetProperties();
-
-        if (properties
+        
+        var properties = modelType.GetProperties()
             .Where(p => !Attribute.IsDefined(p, typeof(NotTrackedAttribute)))
-            .Any(p => !p.CanRead || !p.CanWrite))
+            .ToList();
+
+        if (properties.Count(p => Attribute.IsDefined(p, typeof(KeyAttribute))) != 1)
+        {
+            return false;
+        }
+        
+        if (!properties.All(p => p.CanRead && p.CanWrite))
         {
             return false;
         }
 
-        if (properties
-                .Count(p => Attribute.IsDefined(p, typeof(KeyAttribute)) && 
-                            !Attribute.IsDefined(p, typeof(NotTrackedAttribute)) ) != 1)
-        {
-            return false;
-        }
-
-        return true;
+        return properties.Select(p => p.PropertyType)
+            .Where(pt => pt != typeof(string))
+            .All(pt => typeof(IParsable<>).IsAssignableFrom(pt));
     }
 }
