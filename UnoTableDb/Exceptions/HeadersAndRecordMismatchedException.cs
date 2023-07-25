ea11c96c51c83b5985d9f91a8c5574a025cbc624
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-07-25 12:28:26 +0300

    Add some Exception classes

---
+++ UnoTableDb/Exceptions/HeadersAndRecordMismatchedException.cs
@@ -0,0 +1,10 @@
+﻿namespace UnoTableDb.Exceptions;
+
+public class HeadersAndRecordMismatchedException : Exception
+{
+    public HeadersAndRecordMismatchedException() { }
+
+    public HeadersAndRecordMismatchedException(string message) : base(message) { }
+    
+    public HeadersAndRecordMismatchedException(string message, Exception inner) : base(message, inner) { }
+}
