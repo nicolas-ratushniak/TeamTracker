4af8fa6cbb01115cbd2b99ba778f6cb519be0437
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-07-25 15:51:42 +0300

    Add ex if property not found during parsing

---
+++ UnoTableDb/Exceptions/PropertyNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace UnoTableDb.Exceptions;
+
+public class PropertyNotFoundException : Exception
+{
+    public PropertyNotFoundException() { }
+
+    public PropertyNotFoundException(string message) : base(message) { }
+    
+    public PropertyNotFoundException(string message, Exception inner) : base(message, inner) { }
+}
