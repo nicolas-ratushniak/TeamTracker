5b835b57d14de84e3c32ec278408968b0f7676ff
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-08-11 18:55:46 +0300

    Add new models

---
+++ TeamTracker.Wpf/Models/Cup.cs
@@ -0,0 +1,10 @@
+﻿using TeamTracker.Wpf.Core;
+
+namespace TeamTracker.Wpf.Models;
+
+public class Cup : BaseModel
+{
+    public string Name { get; set; } = "";
+    public Guid? WinnerTeam { get; set; }
+    public bool IsFinished { get; set; }
+}
---
+++ TeamTracker.Wpf/Models/GameInfo.cs
@@ -0,0 +1,13 @@
+﻿using TeamTracker.Wpf.Core;
+
+namespace TeamTracker.Wpf.Models;
+
+public class GameInfo : BaseModel
+{
+    public Guid CupId { get; set; }
+    public Guid TeamHomeId { get; set; }
+    public Guid TeamAwayId { get; set; }
+    public int TeamHomeScore { get; set; }
+    public int TeamAwayScore { get; set; }
+    public bool IsFinished { get; set; }
+}
