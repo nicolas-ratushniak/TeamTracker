811f265d283782a9e64cbf0440fd3b891c480be7
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-08-11 09:21:06 +0300

    Edit the logic of attribute for tracking properties

--- TeamTracker.Wpf/Data/Attributes/TrackedAttribute.cs
+++ TeamTracker.Wpf/Data/Attributes/TrackedAttribute.cs
@@ -1,6 +1,6 @@
 ﻿namespace TeamTracker.Wpf.Data.Attributes;
 
-public class NotTrackedAttribute : Attribute
+public class TrackedAttribute : Attribute
 {
     
 }
--- TeamTracker.Wpf/Models/Team.cs
+++ TeamTracker.Wpf/Models/Team.cs
@@ -5,13 +5,18 @@ namespace TeamTracker.Wpf.Models;
 
 public class Team : BaseModel
 {
+    [Tracked] 
     public string Name { get; set; } = "";
+    [Tracked] 
     public string OriginCity { get; set; } = "";
+    [Tracked] 
     public int TimesWon { get; set; }
+    [Tracked] 
     public int TimesLost { get; set; }
+    [Tracked] 
     public int TimesDraw { get; set; }
+    [Tracked] 
     public int MembersCount { get; set; }
-
-    [NotTracked] 
+    
     public int TotalGames => TimesWon + TimesLost + TimesDraw;
 }
