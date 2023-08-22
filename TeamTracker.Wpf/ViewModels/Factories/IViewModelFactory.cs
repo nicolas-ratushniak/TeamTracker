44d53b49e737f8191832b41b2ba4a201721a53cf
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-08-22 15:09:09 +0300

    Add ViewModelFactory

---
+++ TeamTracker.Wpf/ViewModels/Factories/IViewModelFactory.cs
@@ -0,0 +1,8 @@
+﻿using TeamTracker.Wpf.Navigation;
+
+namespace TeamTracker.Wpf.ViewModels.Factories;
+
+public interface IViewModelFactory
+{
+    public ViewModelBase CreateViewModel(ViewType viewType);
+}
---
+++ TeamTracker.Wpf/ViewModels/Factories/ViewModelFactory.cs
@@ -0,0 +1,27 @@
+﻿using TeamTracker.Domain.Services;
+using TeamTracker.Wpf.Navigation;
+
+namespace TeamTracker.Wpf.ViewModels.Factories;
+
+public class ViewModelFactory : IViewModelFactory
+{
+    private readonly ITeamService _teamService;
+    private readonly IGameInfoService _gameInfoService;
+
+    public ViewModelFactory(ITeamService teamService, IGameInfoService gameInfoService)
+    {
+        _teamService = teamService;
+        _gameInfoService = gameInfoService;
+    }
+
+    public ViewModelBase CreateViewModel(ViewType viewType)
+    {
+        return viewType switch
+        {
+            ViewType.Teams => new TeamsViewModel(_teamService),
+            ViewType.Games => new GamesViewModel(_gameInfoService),
+            ViewType.Help => new HelpViewModel(),
+            _ => throw new InvalidOperationException("Cannot create nonexistent view model")
+        };
+    }
+}
