ee4407953810d76613578d28120a33bf8cc3fa37
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-08-22 15:10:39 +0300

    Create MainViewModel to hold a switching view

--- TeamTracker.Wpf/MainWindow.xaml.cs
+++ TeamTracker.Wpf/MainWindow.xaml.cs
@@ -1,28 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using TeamTracker.Wpf.ViewModels;
 
-namespace TeamTracker.Wpf
+namespace TeamTracker.Wpf;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow(ViewModelBase dataContextViewModel)
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        DataContext = dataContextViewModel;
     }
 }
---
+++ TeamTracker.Wpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using TeamTracker.Wpf.Navigation;
+using TeamTracker.Wpf.ViewModels.Factories;
+
+namespace TeamTracker.Wpf.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    private readonly IViewModelFactory _viewModelFactory;
+    private ViewModelBase _currentViewModel;
+
+    public INavigator Navigator { get; }
+
+    public ViewModelBase CurrentViewModel
+    {
+        get => _currentViewModel;
+        set
+        {
+            if (Equals(value, _currentViewModel)) return;
+            _currentViewModel = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public MainViewModel(INavigator navigator, IViewModelFactory viewModelFactory)
+    {
+        _viewModelFactory = viewModelFactory;
+        Navigator = navigator;
+        
+        Navigator.CurrentViewTypeChanged += Navigator_OnCurrentViewTypeChanged;
+        Navigator.SetCurrentViewType(ViewType.Teams);
+    }
+
+    private void Navigator_OnCurrentViewTypeChanged(object? sender, EventArgs args)
+    {
+        var viewTypeChangedArgs = (ViewTypeChangedEventArgs)args;
+        CurrentViewModel = _viewModelFactory.CreateViewModel(viewTypeChangedArgs.NewViewType);
+    }
+}
