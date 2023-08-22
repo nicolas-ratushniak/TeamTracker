9550e308455f9f75a482f93f1fb6396c3deb92bd
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-08-22 15:12:17 +0300

    Add some more VMs

---
+++ TeamTracker.Wpf/ViewModels/TeamDetailsViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace TeamTracker.Wpf.ViewModels;
+
+public class TeamDetailsViewModel : ViewModelBase
+{
+    
+}
---
+++ TeamTracker.Wpf/ViewModels/TeamHeaderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using TeamTracker.Domain.Models;
+
+namespace TeamTracker.Wpf.ViewModels;
+
+public class TeamHeaderViewModel : ViewModelBase
+{
+    public Guid Id { get; }
+    public string FullName { get; }
+
+    public TeamHeaderViewModel(Team team)
+    {
+        Id = team.Id;
+        FullName = $"{team.Name}-{team.OriginCity}";
+    }
+}
