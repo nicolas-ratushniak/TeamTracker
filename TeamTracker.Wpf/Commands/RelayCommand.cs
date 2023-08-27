e5833f19fe8ad2d3a2e6644a444a639070b68ec2
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-08-27 16:44:18 +0300

    Edit relay command

--- TeamTracker.Wpf/Commands/RelayCommand.cs
+++ TeamTracker.Wpf/Commands/RelayCommand.cs
@@ -7,17 +7,20 @@ public class RelayCommand<T> : ICommand
     private readonly Action<T> _execute;
     private readonly Predicate<T>? _canExecute;
     
-    public bool CanExecute(object parameter)
+    public bool CanExecute(object? parameter)
     {
-        return _canExecute is null || _canExecute((T)parameter);
+        return _canExecute is null || _canExecute((T)parameter!);
     }
 
-    public void Execute(object parameter)
+    public void Execute(object? parameter)
     {
-        _execute((T)parameter);
+        _execute((T)parameter!);
     }
 
-    public event EventHandler? CanExecuteChanged;
+    public event EventHandler? CanExecuteChanged {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
 
     public RelayCommand(Action<T> execute, Predicate<T>? canExecute = null)
     {
