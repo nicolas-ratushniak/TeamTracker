9289e62cff389b342f146bab5200ca4793f45b6a
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-08-29 10:58:54 +0300

    Add PlaceholderTextBox

---
+++ TeamTracker.Wpf/Controls/PlaceholderTextBox.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TeamTracker.Wpf.Controls;
+
+public partial class PlaceholderTextBox : UserControl
+{
+    public static readonly DependencyProperty TextProperty;
+    public static readonly DependencyProperty PlaceholderProperty;
+    public static readonly DependencyProperty IsTypingProperty;
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+    
+    public string Placeholder
+    {
+        get => (string)GetValue(PlaceholderProperty);
+        set => SetValue(PlaceholderProperty, value);
+    }
+    
+    private bool IsTyping
+    {
+        get => (bool)GetValue(IsTypingProperty);
+        set => SetValue(IsTypingProperty, value);
+    }
+
+
+    static PlaceholderTextBox()
+    {
+        TextProperty = DependencyProperty.Register(nameof(Text), typeof(string), typeof(PlaceholderTextBox),
+            new PropertyMetadata(OnTextChanged));
+        PlaceholderProperty = DependencyProperty.Register(nameof(Placeholder), typeof(string), typeof(PlaceholderTextBox));
+        IsTypingProperty = DependencyProperty.Register(nameof(IsTyping), typeof(bool), typeof(PlaceholderTextBox));
+    }
+
+    public PlaceholderTextBox()
+    {
+        InitializeComponent();
+    }
+
+    private static void OnTextChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+    {
+        var control = (PlaceholderTextBox)sender;
+        var newText = (string)e.NewValue;
+
+        control.IsTyping = !string.IsNullOrEmpty(newText);
+    }
+}
---
+++ TeamTracker.Wpf/Converters/InverseBoolToVisibilityConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace TeamTracker.Wpf.Converters;
+
+public class InverseBoolToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (bool)value ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
