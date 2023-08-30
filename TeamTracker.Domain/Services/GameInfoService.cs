3cf806955a3e81689c6a9ff90b53adb426897c32
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-08-30 21:24:12 +0300

    Fix bug in GameInfoService
    Created game is not saved

--- TeamTracker.Domain/Services/GameInfoService.cs
+++ TeamTracker.Domain/Services/GameInfoService.cs
@@ -58,6 +58,8 @@ public class GameInfoService : IGameInfoService
         };
         
         _gamesRepository.Add(game);
+        _gamesRepository.SaveChanges();
+        
         UpdateTeamsStats(game);
     }
 
