d625b3f532f0fdfde6d827643ebca232821dccd5
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-09-03 13:23:55 +0300

    Improve text fields validation

--- TeamTracker.Domain/Dto/TeamUpdateDto.cs
+++ TeamTracker.Domain/Dto/TeamUpdateDto.cs
@@ -8,14 +8,18 @@ public class TeamUpdateDto
     public Guid Id { get; set; }
     
     [Required]
-    [StringLength(30, MinimumLength = 2)]
+    [StringLength(20, MinimumLength = 2, ErrorMessage = "Name field length should be in range from 2 to 20")]
+    [RegularExpression(@"^[A-Z]([\s\-]?[a-zA-Z\d])*$", 
+        ErrorMessage = "Name field can only contain letters, digits and \"-\". It should start with a capital letter and end with no space")]
     public string Name { get; set; } = "";
     
     [Required]
-    [StringLength(40, MinimumLength = 2)]
+    [StringLength(20, MinimumLength = 2, ErrorMessage = "Origin City field length should be in range from 2 to 20")]
+    [RegularExpression(@"^[A-Z]([\s\-]?[a-zA-Z\d])*$", 
+        ErrorMessage = "Origin City field can only contain letters, digits and \"-\". It should start with a capital letter and end with no space")]
     public string OriginCity { get; set; } = "";
     
     [Required]
-    [Range(0, int.MaxValue)]
+    [Range(1, 900, ErrorMessage = "Members value should be in range from 1 to 900")]
     public int MembersCount { get; set; }
 }
