c6852607231155bbc3b5283d25399ea4940411a8
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-10-03 17:40:02 +0300

    Edit converters

--- TeamTracker.Wpf/Converters/InverseBoolToVisibilityConverter.cs
+++ TeamTracker.Wpf/Converters/InverseBoolToVisibilityConverter.cs
@@ -14,6 +14,6 @@ public class InverseBoolToVisibilityConverter : IValueConverter
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        return value;
+        return Binding.DoNothing;
     }
 }
